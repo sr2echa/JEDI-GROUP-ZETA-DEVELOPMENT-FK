0b959dbeb7740eb8cf78222a04d99eba22934fb5
sr2echa <65058816+sr2echa@users.noreply.github.com>
2026-01-29 07:49:39 +0530

    init dropwizard

---
+++ JEDI-DEMO-PRACTICE-PROJECT/DROPWIZARD/JEDI_ZETA_DEVELOPMENT_JAVA_POS_DAO/src/com/flipfit/exception/RegistrationFailedException.java
@@ -0,0 +1,22 @@
+package com.flipfit.exception;
+
+/// Class level Commenting
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class RegistrationFailedException.
+ *
+ * @author Zeta
+ * @ClassName  "RegistrationFailedException"
+ */
+public class RegistrationFailedException extends Exception {
+    
+    /**
+     * Instantiates a new registration failed exception.
+     *
+     * @param message the message
+     */
+    public RegistrationFailedException(String message) {
+        super(message);
+    }
+}
